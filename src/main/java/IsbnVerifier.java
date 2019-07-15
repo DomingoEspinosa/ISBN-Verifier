8f796bd502fc124c688b821e0b20f93fae90de1f
Domingo Espinosa <domingo.espinosa@codurance.com>
2019-07-15 09:27:55 +0200

    Second commit

---
+++ src/main/java/IsbnVerifier.java
@@ -0,0 +1,21 @@
+class IsbnVerifier {
+    //(x1 * 10 + x2 * 9 + x3 * 8 + x4 * 7 + x5 * 6 + x6 * 5 + x7 * 4 + x8 * 3 + x9 * 2 + x10 * 1) mod 11 == 0
+    //( 3 * 10 + 5  * 9 + 9  * 8 + 8  * 7 + 2  * 6 + 1  * 5 + 5  * 4 + 0  * 3 + 8  * 2 + 8   * 1) mod 11 == 0
+    //
+    //3-598-21508-8
+    private int[] multiplier = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    boolean isValid(String stringToVerify) {
+        String replace = stringToVerify.replace("-", "");
+        String[] stringsInArray = replace.split("");
+        int result = 0;
+        for (int i = 0; i < stringsInArray.length; i++) {
+            int value = Integer.valueOf(stringsInArray[i]);
+            result += value * multiplier[i];
+        }
+        return result % 11 == 0;
+
+    }
+
+}
+
